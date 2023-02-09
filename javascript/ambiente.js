5c2c44dc14d5e885aac63b7e7517c1b2cb0a706f
Rodjfreitas <rodrigofreitas2011@live.com>
2023-02-09 18:42:17 -0300

    feat: js CRUD

--- javascript/ambiente.js
+++ javascript/ambiente.js
@@ -8,7 +8,7 @@ function selectOption(){
   }else {
     inputEstrangeiro.style.visibility = "hidden"
   }
-  console.log(option)
+  
 }
 
 
@@ -18,24 +18,28 @@ let age = document.querySelector('#age')
 let naturalidade = document.querySelector('#naturalidade')
 let natOutra = document.querySelector('#natOutra')
 let city = document.querySelector('#city')
+let state = document.querySelector('#state')
 let rg = document.querySelector('#rg')
 let cpf = document.querySelector('#cpf')
 let tel = document.querySelector('#tel')
-let bin = document.createElement('img')
-bin.setAttribute('id','bin')
-bin.value = `\u274C`
+
+
+
+
+
 
     if(name.value.length === 0 || age.value.length === 0 || naturalidade.value.length === 0 ||
         city.value.length === 0 || rg.value.length === 0 || cpf.value.length === 0 || tel.value.length === 0){
       alert(`[ERRO:] Todos os campos precisam ser preenchidos.`)
     }else{
       
-      let cadastro = [name,age,naturalidade,city,rg,cpf,tel,bin]
+      let cadastro = [name, age, naturalidade, city, state, rg, cpf, tel]
       let tabela = document.querySelector('#lancamentos')
       let linha = document.createElement('tr')
       
 
       for(let pos in cadastro){
+        
         let list = document.createElement('td')
         list.setAttribute('id','list')
         list.append(cadastro[pos].value)
@@ -44,10 +48,33 @@ bin.value = `\u274C`
         cadastro[pos].value = ""
 
       }
-      
+
+      let bin = document.createElement('td')
+      bin.setAttribute('id','bin')
+      bin.setAttribute('onclick','removeLinha(this)')
+      bin.append(`\u274C`)
+      linha.append(bin)
+
       
       tabela.append(linha)
+      naturalidade.value = "Brasileira"
 
 
     }
 }
+
+
+function removeLinha(element){
+  let dialog = confirm("Tem certeza que deseja excluir?")
+  if(dialog){
+    var el = element.closest('tr');
+    var index = Array.prototype.indexOf.call(el.parentNode.children, el);
+    console.log(index); // mostrar posição da linha como exemplo
+    //removo a linha
+    el.outerHTML = '';
+  }else{
+    
+  }
+
+}
+
